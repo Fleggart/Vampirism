ecebf2a47dde92f91aaf860e42479089b5f580b6
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-08-29 07:18:09 -0400

    Update HunterSkills.java

--- src/main/java/de/teamlapen/vampirism/player/hunter/skills/HunterSkills.java
+++ src/main/java/de/teamlapen/vampirism/player/hunter/skills/HunterSkills.java
@@ -32,19 +32,9 @@ public class HunterSkills {
     public static final ISkill tech_weapons = UtilLib.getNull();
     public static final ISkill stake1 = UtilLib.getNull();
     public static final ISkill stake2 = UtilLib.getNull();
-    public static final ISkill blood_potion_table = UtilLib.getNull();
-    public static final ISkill blood_potion_less_bad = UtilLib.getNull();
-    public static final ISkill blood_potion_good_or_bad = UtilLib.getNull();
-    public static final ISkill blood_potion_identify_some = UtilLib.getNull();
-    public static final ISkill blood_potion_category_hint = UtilLib.getNull();
-    public static final ISkill blood_potion_less_bad_2 = UtilLib.getNull();
-    public static final ISkill blood_potion_faster_crafting = UtilLib.getNull();
-    public static final ISkill blood_potion_portable_crafting = UtilLib.getNull();
-    public static final ISkill blood_potion_duration = UtilLib.getNull();
     public static final ISkill basic_alchemy = UtilLib.getNull();
     public static final ISkill garlic_beacon = UtilLib.getNull();
     public static final ISkill holy_water_enhanced = UtilLib.getNull();
-    // 移除 purified_garlic 声明
     public static final ISkill garlic_beacon_improved = UtilLib.getNull();
     public static final ISkill hunter_attack_speed = UtilLib.getNull();
     public static final ISkill hunter_advanced_attack_speed = UtilLib.getNull();
@@ -83,30 +73,12 @@ public class HunterSkills {
                 return desc;
             }
         });
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_table", 64, 32, true));
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_less_bad", 80, 32, true));
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_good_or_bad", 96, 32, true));
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_identify_some", 112, 32, true));
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_category_hint", 128, 32, true));
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_less_bad_2", 80, 32, true) {
-            @Override
-            public String getLocalizedDescription() {
-                return UtilLib.translate("text.vampirism.skill.blood_potion_less_bad.desc");
-            }
 
-            @Override
-            public String getUnlocalizedName() {
-                return "text.vampirism.skill.blood_potion_less_bad";
-            }
-        });
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_faster_crafting", 144, 32, false));
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_portable_crafting", 176, 32, true));
-        registry.register(new VampirismSkill.SimpleHunterSkill("blood_potion_duration", 160, 32, true));
         registry.register(new VampirismSkill.SimpleHunterSkill("basic_alchemy", 32, 48, true));
         registry.register(new VampirismSkill.SimpleHunterSkill("garlic_beacon", 48, 48, true));
         registry.register(new VampirismSkill.SimpleHunterSkill("holy_water_enhanced", 80, 48, true));
-        // 移除 purified_garlic 技能注册
         registry.register(new VampirismSkill.SimpleHunterSkill("garlic_beacon_improved", 96, 48, true));
+
         DefaultSkill<IHunterPlayer> attackSpeed = new VampirismSkill.SimpleHunterSkill("hunter_attack_speed", 32, 32, false);
         attackSpeed.registerAttributeModifier(SharedMonsterAttributes.ATTACK_SPEED, "8dd2f8cc-6ae1-4db1-9e14-96b4c74d7bf2", Balance.hps.SMALL_ATTACK_SPEED_MODIFIER, 2);
 
@@ -130,18 +102,15 @@ public class HunterSkills {
         ISkillManager skillManager = VampirismAPI.skillManager();
         SkillNode skill2 = skillManager.createSkillNode(root, stake1);
 
-
         SkillNode skill3 = skillManager.createSkillNode(skill2, hunter_attack_speed);
         SkillNode skill4 = skillManager.createSkillNode(skill3, hunter_disguise);
         registerAlchemy(skillManager, skill4);
-        registerBloodAlchemy(skillManager, skill4);
         registerWeaponSkills(skillManager, skill4);
     }
 
 
     private static void registerWeaponSkills(ISkillManager skillManager, SkillNode root) {
         SkillNode skill5 = skillManager.createSkillNode(root, weapon_table);
-
 
         SkillNode skill6 = skillManager.createSkillNode(skill5, hunter_advanced_attack_speed, double_crossbow);
         SkillNode skill7 = skillManager.createSkillNode(skill6, enhanced_weapons, enhanced_crossbow);
@@ -153,18 +122,8 @@ public class HunterSkills {
     private static void registerAlchemy(ISkillManager skillManager, SkillNode root) {
         SkillNode skill5 = skillManager.createSkillNode(root, basic_alchemy);
         SkillNode skill6 = skillManager.createSkillNode(skill5, garlic_beacon);
-        // 移除 purified_garlic 依赖
         SkillNode skill7 = skillManager.createSkillNode(skill6, holy_water_enhanced);
         SkillNode skill8 = skillManager.createSkillNode(skill7, garlic_beacon_improved);
         SkillNode skill9 = skillManager.createSkillNode(skill8, hunter_awareness);
-    }
-
-    private static void registerBloodAlchemy(ISkillManager skillManager, SkillNode root) {
-        SkillNode skill5 = skillManager.createSkillNode(root, blood_potion_table);
-        SkillNode skill6 = skillManager.createSkillNode(skill5, blood_potion_less_bad, blood_potion_good_or_bad);
-        SkillNode skill7 = skillManager.createSkillNode(skill6, blood_potion_faster_crafting, blood_potion_category_hint);
-        SkillNode skill8 = skillManager.createSkillNode(skill7, blood_potion_duration);
-        SkillNode skill9 = skillManager.createSkillNode(skill8, blood_potion_portable_crafting);
-        SkillNode skill10 = skillManager.createSkillNode(skill9, blood_potion_less_bad_2, blood_potion_identify_some);
     }
 }
