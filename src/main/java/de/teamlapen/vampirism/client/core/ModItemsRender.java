5a90ae10db95123ce26ca658da1cee15459e782a
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-01 12:20:07 +0800

    Add files via upload

--- src/main/java/de/teamlapen/vampirism/client/core/ModItemsRender.java
+++ src/main/java/de/teamlapen/vampirism/client/core/ModItemsRender.java
@@ -1,4 +1,4 @@
-// ModItemsRender.java - 移除 hunter_hat 渲染注册
+// ModItemsRender.java - 移除 holy_water_bottle 渲染注册
 package de.teamlapen.vampirism.client.core;
 
 import de.teamlapen.lib.lib.util.InventoryRenderHelper;
@@ -28,96 +28,92 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class ModItemsRender {
 
-	public static void register() {
-		registerRenderers();
-	}
+    public static void register() {
+        registerRenderers();
+    }
 
-	static void registerColors() {
+    static void registerColors() {
 
+        Minecraft.getMinecraft().getItemColors().registerItemColorHandler((stack, tintIndex) -> {
+            if (tintIndex == 1) {
+                return ItemCrossbowArrow.getType(stack).color;
+            }
+            return 0xFFFFFF;
+        }, ModItems.crossbow_arrow);
+    }
 
-		Minecraft.getMinecraft().getItemColors().registerItemColorHandler((stack, tintIndex) -> {
-			if (tintIndex == 1) {
-				return ItemCrossbowArrow.getType(stack).color;
-			}
-			return 0xFFFFFF;
-		}, ModItems.crossbow_arrow);
-	}
-
-	private static void registerRenderers() {
-		VampirismMod.log.d("ModItemsRender", "Registering renderer");
-		InventoryRenderHelper renderHelper = new InventoryRenderHelper(REFERENCE.MODID);
-		renderHelper.registerRender(ModItems.vampire_fang, "normal");
-		renderHelper.registerRender(ModItems.human_heart, "normal");
-		renderHelper.registerRender(ModItems.weak_human_heart, "normal");
-		renderHelper.registerRender(ModItems.item_tent, "normal");
-		renderHelper.registerRenderAllMeta(ModItems.blood_bottle, ItemBloodBottle.AMOUNT + 1);
-		renderHelper.registerRender(ModItems.item_coffin, "normal");
-		renderHelper.registerRenderAllMeta(ModItems.pure_blood, ItemPureBlood.COUNT);
-		renderHelper.registerRenderAllMeta(ModItems.hunter_intel, HunterLevelingConf.instance().HUNTER_INTEL_COUNT, "normal");
-		renderHelper.registerRender(ModItems.item_garlic, "normal");
-		renderHelper.registerRenderAllMeta(ModItems.injection, ItemInjection.META_COUNT);
-		renderHelper.registerRender(ModItems.item_med_chair, "normal");
-		renderHelper.registerRender(ModItems.basic_crossbow, "normal");
-		renderHelper.registerRender(ModItems.crossbow_arrow, "normal");
-		renderHelper.registerRender(ModItems.basic_double_crossbow, "normal");
-		renderHelper.registerRender(ModItems.enhanced_crossbow, "normal");
-		renderHelper.registerRender(ModItems.enhanced_double_crossbow, "normal");
-		renderHelper.registerRender(ModItems.stake, "normal");
-		renderHelper.registerRender(ModItems.vampire_blood_bottle, "normal");
-		renderHelper.registerRender(ModItems.blood_potion, "normal");
-		renderHelper.registerRender(ModItems.basic_tech_crossbow, "normal");
-		renderHelper.registerRender(ModItems.enhanced_tech_crossbow, "normal");
-		renderHelper.registerRender(ModItems.tech_crossbow_ammo_package, "normal");
-		renderHelper.registerRender(ModItems.vampire_book, "normal");
-		registerSimpleItemWithTier(ModItems.holy_water_bottle);
-		renderHelper.registerRender(ModItems.holy_salt, "normal");
-		renderHelper.registerRender(ModItems.holy_salt_water, "normal");
-		registerSimpleItemWithTier(ModItems.holy_water_bottle);
-		registerSimpleItemWithTier(ModItems.holy_water_splash_bottle);
-		renderHelper.registerRender(ModItems.blood_infused_iron_ingot, "normal");
-		renderHelper.registerRender(ModItems.blood_infused_enhanced_iron_ingot, "normal");
-		renderHelper.registerRender(ModItems.soul_orb_vampire, "normal");
+    private static void registerRenderers() {
+        VampirismMod.log.d("ModItemsRender", "Registering renderer");
+        InventoryRenderHelper renderHelper = new InventoryRenderHelper(REFERENCE.MODID);
+        renderHelper.registerRender(ModItems.vampire_fang, "normal");
+        renderHelper.registerRender(ModItems.human_heart, "normal");
+        renderHelper.registerRender(ModItems.weak_human_heart, "normal");
+        renderHelper.registerRender(ModItems.item_tent, "normal");
+        renderHelper.registerRenderAllMeta(ModItems.blood_bottle, ItemBloodBottle.AMOUNT + 1);
+        renderHelper.registerRender(ModItems.item_coffin, "normal");
+        renderHelper.registerRenderAllMeta(ModItems.pure_blood, ItemPureBlood.COUNT);
+        renderHelper.registerRenderAllMeta(ModItems.hunter_intel, HunterLevelingConf.instance().HUNTER_INTEL_COUNT, "normal");
+        renderHelper.registerRender(ModItems.item_garlic, "normal");
+        renderHelper.registerRenderAllMeta(ModItems.injection, ItemInjection.META_COUNT);
+        renderHelper.registerRender(ModItems.item_med_chair, "normal");
+        renderHelper.registerRender(ModItems.basic_crossbow, "normal");
+        renderHelper.registerRender(ModItems.crossbow_arrow, "normal");
+        renderHelper.registerRender(ModItems.basic_double_crossbow, "normal");
+        renderHelper.registerRender(ModItems.enhanced_crossbow, "normal");
+        renderHelper.registerRender(ModItems.enhanced_double_crossbow, "normal");
+        renderHelper.registerRender(ModItems.stake, "normal");
+        renderHelper.registerRender(ModItems.vampire_blood_bottle, "normal");
+        renderHelper.registerRender(ModItems.blood_potion, "normal");
+        renderHelper.registerRender(ModItems.basic_tech_crossbow, "normal");
+        renderHelper.registerRender(ModItems.enhanced_tech_crossbow, "normal");
+        renderHelper.registerRender(ModItems.tech_crossbow_ammo_package, "normal");
+        renderHelper.registerRender(ModItems.vampire_book, "normal");
+        // registerSimpleItemWithTier(ModItems.holy_water_bottle);  // 已移除
+        renderHelper.registerRender(ModItems.holy_salt, "normal");
+        renderHelper.registerRender(ModItems.holy_salt_water, "normal");
+        // registerSimpleItemWithTier(ModItems.holy_water_splash_bottle);  // 已移除
+        renderHelper.registerRender(ModItems.blood_infused_iron_ingot, "normal");
+        renderHelper.registerRender(ModItems.blood_infused_enhanced_iron_ingot, "normal");
+        renderHelper.registerRender(ModItems.soul_orb_vampire, "normal");
         registerVampireCloakWithColor(ModItems.vampire_cloak, "vampire_cloak");
-		renderHelper.registerRender(ModItems.garlic_bread, "normal");
+        renderHelper.registerRender(ModItems.garlic_bread, "normal");
+    }
 
-		// ----------------------
-	}
+    /**
+     * Registers all variants of an {@link IItemWithTier} as well as the custom mesh
+     * definition Only works with items that only have variants based on tier
+     */
+    private static void registerSimpleItemWithTier(IItemWithTier itemWithTier) {
+        Item item = (Item) itemWithTier;
+        ResourceLocation loc = new ResourceLocation(REFERENCE.MODID, "item/" + item.getRegistryName().getPath());
+        ModelLoader.setCustomMeshDefinition(item, new IItemWithTier.SimpleMeshDefinition(loc));
+        for (IStringSerializable s : IItemWithTier.TIER.values()) {
+            ModelLoader.registerItemVariants(item, new ModelResourceLocation(loc, "tier=" + s.getName()));
+        }
+    }
 
-	/**
-	 * Registers all variants of an {@link IItemWithTier} as well as the custom mesh
-	 * definition Only works with items that only have variants based on tier
-	 */
-	private static void registerSimpleItemWithTier(IItemWithTier itemWithTier) {
-		Item item = (Item) itemWithTier;
-		ResourceLocation loc = new ResourceLocation(REFERENCE.MODID, "item/" + item.getRegistryName().getPath());
-		ModelLoader.setCustomMeshDefinition(item, new IItemWithTier.SimpleMeshDefinition(loc));
-		for (IStringSerializable s : IItemWithTier.TIER.values()) {
-			ModelLoader.registerItemVariants(item, new ModelResourceLocation(loc, "tier=" + s.getName()));
-		}
-	}
+    /**
+     * Registers all variants of an {@link IItemWithTier} ItemArmor as well as the
+     * custom mesh definition Only works with items that only have variants based on
+     * tier
+     */
+    private static void registerArmorItemWithTier(IItemWithTier armorWithTier, String baseName) {
+        ItemArmor item = (ItemArmor) armorWithTier;
+        ResourceLocation loc = new ResourceLocation(REFERENCE.MODID, "item/" + baseName);
+        ModelLoader.setCustomMeshDefinition(item, new IItemWithTier.ArmorMeshDefinition(loc));
+        for (IStringSerializable s : IItemWithTier.TIER.values()) {
+            ModelLoader.registerItemVariants(item, new ModelResourceLocation(loc, "part=" + item.armorType.getName() + "_" + s.getName()));
+        }
+    }
 
-	/**
-	 * Registers all variants of an {@link IItemWithTier} ItemArmor as well as the
-	 * custom mesh definition Only works with items that only have variants based on
-	 * tier
-	 */
-	private static void registerArmorItemWithTier(IItemWithTier armorWithTier, String baseName) {
-		ItemArmor item = (ItemArmor) armorWithTier;
-		ResourceLocation loc = new ResourceLocation(REFERENCE.MODID, "item/" + baseName);
-		ModelLoader.setCustomMeshDefinition(item, new IItemWithTier.ArmorMeshDefinition(loc));
-		for (IStringSerializable s : IItemWithTier.TIER.values()) {
-			ModelLoader.registerItemVariants(item, new ModelResourceLocation(loc, "part=" + item.armorType.getName() + "_" + s.getName()));
-		}
-	}
-
-	/**
+    /**
      * Register all variants of an Item based on {@link EnumCloakColor} Only works
      * with vampirecloaks
      */
     private static void registerVampireCloakWithColor(Item item, String baseName) {
         for (EnumCloakColor e : EnumCloakColor.values()) {
-			ModelLoader.setCustomModelResourceLocation(item, e.getMetadata(), new ModelResourceLocation(new ResourceLocation(REFERENCE.MODID, "item/" + baseName), "color=" + e.getDyeColorName()));
-		}
-	}
+            ModelLoader.setCustomModelResourceLocation(item, e.getMetadata(), new ModelResourceLocation(new ResourceLocation(REFERENCE.MODID, "item/" + baseName), "color=" + e.getDyeColorName()));
+        }
+    }
 
 }
