a8cc120b6ce786b9363ad03c5ed532bebf0f0182
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-01 19:53:19 +0800

    Remove IVillageCaptureEntity from IBasicVampire
    Removed IVillageCaptureEntity import and interface extension from IBasicVampire.

--- src/api/java/de/teamlapen/vampirism/api/entity/vampire/IBasicVampire.java
+++ src/api/java/de/teamlapen/vampirism/api/entity/vampire/IBasicVampire.java
@@ -1,10 +1,12 @@
 package de.teamlapen.vampirism.api.entity.vampire;
 
 import de.teamlapen.vampirism.api.difficulty.IAdjustableLevel;
-import de.teamlapen.vampirism.api.entity.IVillageCaptureEntity;
+// 删除 IVillageCaptureEntity 导入
+// import de.teamlapen.vampirism.api.entity.IVillageCaptureEntity;
 
 /**
  * Interface for the basic vampire mob
+ * Village capture functionality has been removed.
  */
-public interface IBasicVampire extends IVampireMob, IAdjustableLevel, IVillageCaptureEntity {
+public interface IBasicVampire extends IVampireMob, IAdjustableLevel { // 移除 , IVillageCaptureEntity
 }
