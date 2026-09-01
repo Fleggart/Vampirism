f906657b044d316700a6e87ff6f2e0a0cdc1b252
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-01 19:50:57 +0800

    Update IBasicHunter.java

--- src/api/java/de/teamlapen/vampirism/api/entity/hunter/IBasicHunter.java
+++ src/api/java/de/teamlapen/vampirism/api/entity/hunter/IBasicHunter.java
@@ -1,19 +1,28 @@
 package de.teamlapen.vampirism.api.entity.hunter;
 
 import de.teamlapen.vampirism.api.difficulty.IAdjustableLevel;
-import de.teamlapen.vampirism.api.entity.IVillageCaptureEntity;
+// 删除 IVillageCaptureEntity 导入
+// import de.teamlapen.vampirism.api.entity.IVillageCaptureEntity;
 import net.minecraft.util.math.AxisAlignedBB;
 
 /**
- * Interface for basic
+ * Interface for basic hunter entity.
+ * Village-related methods (makeVillageHunter) are deprecated as village system has been removed.
  */
-public interface IBasicHunter extends IHunterMob, IAdjustableLevel, IVillageCaptureEntity {
+public interface IBasicHunter extends IHunterMob, IAdjustableLevel { // 移除 , IVillageCaptureEntity
+    
     boolean isLookingForHome();
 
     void makeCampHunter(AxisAlignedBB box);
 
     void makeNormalHunter();
 
-    void makeVillageHunter(AxisAlignedBB box);
+    /**
+     * @deprecated Village system has been removed. This method does nothing.
+     */
+    @Deprecated
+    default void makeVillageHunter(AxisAlignedBB box) {
+        // No-op
+    }
 
 }
