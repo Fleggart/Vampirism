d71525d43223fd72f84c73078524911064b4eddd
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-01 15:38:27 +0800

    Update VampirismWorldData.java

--- src/main/java/de/teamlapen/vampirism/world/VampirismWorldData.java
+++ src/main/java/de/teamlapen/vampirism/world/VampirismWorldData.java
@@ -1,18 +1,11 @@
 package de.teamlapen.vampirism.world;
 
-import com.google.common.collect.Lists;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
-import net.minecraft.nbt.NBTUtil;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.storage.WorldSavedData;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.List;
-import java.util.Random;
 
 /**
  * Store all kinds of data which needs to be stored and related to a world
@@ -38,7 +31,6 @@ public class VampirismWorldData extends WorldSavedData {
         return IDENTIFIER + provider.getDimensionType().getSuffix();
     }
 
-    private final List<BlockPos> vampireDungeons = Lists.newLinkedList();
     private World world;
 
     public VampirismWorldData(String name) {
@@ -51,51 +43,14 @@ public class VampirismWorldData extends WorldSavedData {
         this.markDirty();
     }
 
-    /**
-     * Register a new vampire dungeon position during world gen
-     */
-    public void addNewVampireDungeon(@Nonnull BlockPos pos) {
-        vampireDungeons.add(pos);
-        this.markDirty();
-    }
-
-    /**
-     * Return a random vampire dungeon
-     */
-    public @Nullable
-    BlockPos getRandomVampireDungeon(Random rng) {
-        return vampireDungeons.size() == 0 ? null : vampireDungeons.get(rng.nextInt(vampireDungeons.size()));
-    }
-
-    /**
-     * Called when an altar of inspiration is destroyed.
-     * If ther was a vampire dungeon at this postion it is removed
-     */
-    public void onAltarInspirationDestroyed(BlockPos pos) {
-        if (vampireDungeons.remove(pos)) {
-            this.markDirty();
-        }
-    }
-
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
-        if (nbt.hasKey("vampire_dungeons")) {
-            vampireDungeons.clear();
-            NBTTagList dungeons = nbt.getTagList("vampire_dungeons", 10);
-            for (int i = 0; i < dungeons.tagCount(); i++) {
-                NBTTagCompound tag = dungeons.getCompoundTagAt(i);
-                vampireDungeons.add(NBTUtil.getPosFromTag(tag));
-            }
-        }
+        // No data to read currently
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-        NBTTagList dungeons = new NBTTagList();
-        for (BlockPos pos : vampireDungeons) {
-            dungeons.appendTag(NBTUtil.createPosTag(pos));
-        }
-        compound.setTag("vampire_dungeons", dungeons);
+        // No data to write currently
         return compound;
     }
 }
